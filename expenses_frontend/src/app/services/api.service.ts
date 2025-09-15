58aa1934374405b79081f4149ba73260b9f3aa59
Yash <thotayash47@gmail.com>
2025-09-15 16:25:13 +0530

    changes removed / in base url

--- expenses_frontend/src/app/services/api.service.ts
+++ expenses_frontend/src/app/services/api.service.ts
@@ -14,7 +14,7 @@ export class ApiService {
       headers: this.getAuthHeaders()
     });
   }
-  private readonly baseUrl = 'https://expense-share-1.onrender.com/';
+  private readonly baseUrl = 'https://expense-share-1.onrender.com';
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser$ = this.currentUserSubject.asObservable();
 
